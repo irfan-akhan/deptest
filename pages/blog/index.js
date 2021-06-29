87b83c70c820a7f095dff5ab50704967f8a10a8b
Irfan Khan <officialirfanafzal@gmail.com>
2021-06-29 14:04:21 +0530

    added direct Url endpoint

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,3 +1,8 @@
+import dbConnect from '../../utils/db';
+import Post from '../api/post.model';
+
+dbConnect();
+
 const BlogList = ({ posts }) => {
 	console.log(posts);
 	return (
@@ -18,16 +23,25 @@ const BlogList = ({ posts }) => {
 };
 
 export const getStaticProps = async (ctx) => {
-	const response = await fetch('http://localhost:3000/api/posts', {
-		method: 'GET',
-		headers: {
-			// update with your user-agent
-			'User-Agent':
-				'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
-			Accept: 'application/json; charset=UTF-8',
-		},
-	});
-	const { data } = await response.json();
+	// const response = await fetch('http://localhost:3000/api/posts', {
+	// 	method: 'GET',
+	// 	headers: {
+	// 		// update with your user-agent
+	// 		'User-Agent':
+	// 			'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+	// 		Accept: 'application/json; charset=UTF-8',
+	// 	},
+	// });
+	// try {
+	// 	console.log('IN GET');
+	// 	const docs = await Post.find().exec();
+	// 	// res.status(200).json({ success: 'true', data: docs });
+	// 	return JSON.stringify(docs);
+	// } catch (error) {
+	// 	console.log(error);
+	// }
+	const response = await Post.find().exec();
+	const data = await response.json();
 	console.log(data);
 	return {
 		props: {
